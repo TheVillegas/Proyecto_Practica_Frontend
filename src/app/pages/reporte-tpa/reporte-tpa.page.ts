89b00a1fa9aea40b71e4493995543fc733911d19
Matias <matias.villegas.m@mail.pucv.cl>
2025-12-15 17:17:52 -0300

    Pagina TPA etapa 2 realizada
    Tengo la etapa 2 de 5 realizadas

--- src/app/pages/reporte-tpa/reporte-tpa.page.ts
+++ src/app/pages/reporte-tpa/reporte-tpa.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AliService } from 'src/app/services/ali-service';
 import { IonContent } from '@ionic/angular';
@@ -11,10 +11,15 @@ import { IonContent } from '@ionic/angular';
 })
 export class ReporteTPAPage implements OnInit {
 
+  seccionActual: String = '';
   codigoALI: string = '';
   estadoTPA: string = '';
   estadoSeleccionado: string = '';
-  @ViewChild(IonContent, { static: false }) content!: IonContent;
+  listaRepeticionesEtapa2: any[] = [
+    { id: 0, reposable: "", listaMateriales: [{ id: 0, tipoMaterial: "", codigoMaterial: "" }] }
+  ];
+  etapaActual: string = 'etapa1';
+
   constructor(private route: ActivatedRoute, private aliService: AliService) { }
 
   ngOnInit() {
@@ -28,10 +33,46 @@ export class ReporteTPAPage implements OnInit {
     this.estadoSeleccionado = event.detail.value || '';
     console.log(this.estadoSeleccionado);
   }
-  bajarAlFinal() {
-    this.content.scrollToBottom(500);
+
+  siguienteSeccion() {
+    switch (this.seccionActual) {
+      case 'etapa1':
+        this.seccionActual = "etapa2"
+        break;
+      case 'etapa2':
+        this.seccionActual = "etapa3"
+        break;
+      case 'etapa3':
+        this.seccionActual = "etapa4"
+        break;
+      case 'etapa4':
+        this.seccionActual = "etapa5"
+        break;
+      case 'etapa5':
+        this.seccionActual = "etapa1"
+        break;
+    }
   }
-  subirAlInicio() {
-    this.content.scrollToTop(500);
+  agregarRepeticion() {
+    const nuevoID = this.listaRepeticionesEtapa2.length + 1;
+    this.listaRepeticionesEtapa2.push({
+      id: Date.now(),
+      listaMateriales: [
+        { id: Date.now(), tipoMaterial: '', codigoMaterial: '' }
+      ]
+    });
+    console.log(this.listaRepeticionesEtapa2);
   }
+  agregarMaterial(indexAnalista: number) {
+    // Buscamos al analista específico y le empujamos a SU lista
+    this.listaRepeticionesEtapa2[indexAnalista].listaMateriales.push({
+      id: Date.now(),
+      tipoMaterial: '',
+      codigoMaterial: ''
+    });
+  }
+  trackById(index: number, item: any) {
+    return item.id;
+  }
+
 }
